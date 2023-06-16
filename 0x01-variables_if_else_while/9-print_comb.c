9c98ac75e9ae099464253534a6cc626cd06750b3
Mohamed <muhammadamin1916@gmail.com>
2023-06-16 14:25:00 -0700

    Combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Print all possible combinations of single-digit numbers.
+ * Return:0 
+ */
+int main(void)
+{
+
+	int x = 0;
+
+	while (x < 10)
+	{
+		putchar(x);
+		if (x < 9)
+		{
+
+	putchar(',');
+	putchar(' ');
+		}
+
+		x++;
+	}
+
+	putchar('\n');
+	return (0);
+}
