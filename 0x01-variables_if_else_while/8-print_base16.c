25e0ae56a9732368776bf93f2c39c251154295c1
Mohamed <muhammadamin1916@gmail.com>
2023-06-16 13:54:26 -0700

    Print base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,11 +7,11 @@ int main(void)
 {
 	char ch;
 
-	for (ch = 0; ch <= 9; ch ++)
+	for (ch = 0; ch <= 9; ch++)
 	{
 		putchar(ch);
 	}
-	for (ch = 'a'; ch <= 'f'; ch ++)
+	for (ch = 'a'; ch <= 'f'; ch++)
 	{
 		putchar(ch);
 	}
