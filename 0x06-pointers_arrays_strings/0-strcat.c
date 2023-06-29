829572fbd891ceff3e2989630d8bec14ffe476f8
Mohamed <muhammadamin1916@gmail.com>
2023-06-29 12:30:25 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,13 +8,17 @@
 
 char *strcat(char *dest, const char *src)
 {
-	int index = 0, dest_len = 0;
+	int a = -1, i;
 
-	while (dest[index++])
-		dest_len++;
+	for (i = 0; dest[i] != '\0'; i++)
+	;
 
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+	do {
+		a++;
+		dest[i] = src[a];
+		i++;
+	}
+	while (src[a] != '\0');
 
 	return (dest);
 }
