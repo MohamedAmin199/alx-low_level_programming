8719e8be69e6fbec2f5be1d62c11d218151f1b5a
Mohamed <muhammadamin1916@gmail.com>
2023-07-19 15:48:03 -0700

    op

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * op_add - addition.
+ * @a: The first number.
+ * @b: The second number.
+ * Return: result.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtraction.
+ * @a: The first number.
+ * @b: The second number.
+ * Return: result.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplication
+ * @a: The first number.
+ * @b: The second number.
+ * Return: result.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division.
+ * @a: The first number.
+ * @b: The second number.
+ * Return: result.
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - The remainder of the division.
+ * @a: The first number.
+ * @b: The second number.
+ * Return: result.
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
