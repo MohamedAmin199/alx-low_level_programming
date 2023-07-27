ecf179ae51a2ff931a814ea31fbe494fb1c8362e
Mohamed <muhammadamin1916@gmail.com>
2023-07-27 12:05:52 -0700

    first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void _constructor(void) __attribute__ ((constructor));
+
+/**
+ * _constructor - prints a sentence before the main.
+ * Return: void.
+ */
+void _constructor(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
+}
